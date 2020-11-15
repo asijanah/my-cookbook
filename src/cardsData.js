6f822d0d8f1ab640dd8a708243a5642cec932c98
asijanah <asafjanah@gmail.com>
2020-11-15 22:47:07 +0100

    update file img

--- src/cardsData.js
+++ src/cardsData.js
@@ -4,9 +4,9 @@ const cardsData=[
     {id:3,name:'Apple Pie',img:'applepie.jpeg',viewes:0,lastupdate: new Date()},
     {id:4,name:'Omlet',img:'Omlet.jpeg',viewes:0,lastupdate: new Date()},
     {id:5,name:'Pancakes',img:'Pancakes.jpeg',viewes:0,lastupdate: new Date()},
-    {id:6,name:'Shakshuka',img:'recipe-imge.jpeg',viewes:0,lastupdate: new Date()},
+    {id:6,name:'Shakshuka',img:'recipe-imge.jpg',viewes:0,lastupdate: new Date()},
     {id:7,name:'Lemon Pie',img:'lemonpie.jpeg',viewes:0,lastupdate: new Date()},
-    {id:8,name:'Mafrum',img:'mafrum.jpeg',viewes:0,lastupdate: new Date()}
+    {id:8,name:'Mafrum',img:'mafrum.jpg',viewes:0,lastupdate: new Date()}
 ]
 
 export default cardsData
