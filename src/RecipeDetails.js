28014d7ee816126812bd893765c357c157bb4e8f
asijanah <asafjanah@gmail.com>
2020-11-15 11:40:34 +0100

    Add jumbotron

--- src/RecipeDetails.js
+++ src/RecipeDetails.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-function RecipeDetails(){
+function RecipeDetails() {
 
 
 
     return (
         <div id="jambo" className="container mt-3">
-            
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">Fluid jumbotron</h1>
+                    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                </div>
+            </div>
         </div>
     )
 }
+export default RecipeDetails
