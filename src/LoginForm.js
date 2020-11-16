fab98e823e95acc3d86c7f4ef26d0ea8f123f065
asijanah <asafjanah@gmail.com>
2020-11-16 13:00:46 +0100

    Update to JSX attributs

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -3,29 +3,29 @@ import React from 'react';
 function LoginForm() {
     return (
         <div className="card w-50 border-dark card-body">
-            <div class="card-header bg-transparent border-dark">LOG IN</div>
+            <div className="card-header bg-transparent border-dark">LOG IN</div>
             <div className="form" id="login">
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="inputEmail4">Email</label>
+                        <input type="email" className="form-control" id="inputEmail4" placeholder="Email" />
                     </div>
                 </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="inputPassword4">Password</label>
+                        <input type="password" className="form-control" id="inputPassword4" placeholder="Password" />
                     </div>
                 </div>
-                <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
+                <div className="form-group">
+    <div className="form-check">
+      <input className="form-check-input" type="checkbox" id="gridCheck"/>
+      <label className="form-check-label" htmlFor="gridCheck">
       Remember Me
       </label>
     </div>
   </div>
-  <button type="submit" class="btn btn-primary">Log in</button>
+  <button type="submit" className="btn btn-primary">Log in</button>
             </div>
         </div>
     )
