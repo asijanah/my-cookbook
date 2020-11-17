e1c00465390c150052aa541f9a3ff01eead87082
asijanah <asafjanah@gmail.com>
2020-11-17 13:57:06 +0100

    Add all search buttons func to this component

--- src/SearchAndCook.js
+++ src/SearchAndCook.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import cardsData from './cardsData';
 import CookBook from './CookBook';
 
 function SearchAndCook() {
+
+    const [recipes, setRecipes] = useState(cardsData);
+
+    function updateItem(id) {
+        let data = [...cardsData];
+        const recipe = data.find(item => item.id == id);
+        recipe.viewes++;
+        recipe.lastupdate = new Date();
+        
+        setRecipes(data);
+    };
+    
+    const sortByDate=()=>{
+        const newest = [...cardsData];
+        newest.sort((a,b)=> b.lastupdate-a.lastupdate);
+        setRecipes(newest);
+    };
+    
+    const sortByViewes=()=>{
+        const populars = cardsData.slice(0);
+        populars.sort((a,b)=> b.viewes-a.viewes);
+        setRecipes(populars);
+    };
+
     return (
         <div className="main-search">
             <div className="big-header">
@@ -15,12 +40,12 @@ function SearchAndCook() {
                 <div className="form-group firstCheckBtn">
                     <div className="form-check-inline">
                         <label className="form-check-label">
-                            <input type="radio" name="orderProp" className="form-check-input" value="" />Most Popular
+                            <input type="radio" name="orderProp" className="form-check-input" value="" onChange={sortByViewes} />Most Popular
                         </label>
                     </div>
                     <div className="form-check-inline">
                         <label className="form-check-label">
-                            <input type="radio" name="orderProp" className="form-check-input" value="" />Newest
+                            <input type="radio" name="orderProp" className="form-check-input" value="" onChange={sortByDate} />Newest
                         </label>
                     </div>
                 </div>
@@ -43,19 +68,19 @@ function SearchAndCook() {
                     <div className="form-group">
                         <div className="form-check-inline">
                             <label className="form-check-label">
-                                <input type="radio" name="orderProp" className="form-check-input" value="" />Most Popular
+                                <input type="radio" name="orderProp" className="form-check-input" value="" />All
                             </label>
                         </div>
                         <div className="form-check-inline">
                             <label className="form-check-label">
-                                <input type="radio" name="orderProp" className="form-check-input" value="" />Newest
+                                <input type="radio" name="orderProp" className="form-check-input" value="" />At Least one
                             </label>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="row">
-                <CookBook/>
+                <CookBook recipes={recipes} updateItem={updateItem} />
             </div>
         </div>
     )
