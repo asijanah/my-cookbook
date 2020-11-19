71fc9c36c9d1c7a2a1e6dea05c42109080cf7038
asijanah <asafjanah@gmail.com>
2020-11-19 18:54:21 +0100

    Add some Bootstrap style

--- src/cardsData.js
+++ src/cardsData.js
@@ -1,12 +1,12 @@
 const cardsData=[
-    {id:1,name:'Chocolate Cake',img:'chocolate_cake.jpg',viewes:0,lastupdate: new Date()},
-    {id:2,name:'Pizza Margherita',img:'pizza.jpg',viewes:0,lastupdate: new Date()},
-    {id:3,name:'Apple Pie',img:'applepie.jpeg',viewes:0,lastupdate: new Date()},
-    {id:4,name:'Omlet',img:'Omlet.jpeg',viewes:0,lastupdate: new Date()},
-    {id:5,name:'Pancakes',img:'Pancakes.jpeg',viewes:0,lastupdate: new Date()},
-    {id:6,name:'Shakshuka',img:'recipe-imge.jpg',viewes:0,lastupdate: new Date()},
-    {id:7,name:'Lemon Pie',img:'lemonpie.jpeg',viewes:0,lastupdate: new Date()},
-    {id:8,name:'Mafrum',img:'mafrum.jpg',viewes:0,lastupdate: new Date()}
+    {id:1,name:'Chocolate Cake',img:'chocolate_cake.jpg',viewes:0,ingredients:['chocolate',"flour","Baking powder","cocoa"],lastupdate: new Date(), source: 'https://www.bbc.co.uk/food/recipes/easy_chocolate_cake_31070',picture:'Cake.jpeg',type:"desserts"},
+    {id:2,name:'Pizza Margherita',img:'pizza.jpg',viewes:0,ingredients:["cheese","sauce","dough"],lastupdate: new Date(),source:"https://www.bbcgoodfood.com/recipes/pizza-margherita-4-easy-steps"},
+    {id:3,name:'Apple Pie',img:'applepie.jpeg',viewes:0,ingredients:["apples","suger","water","oil"],lastupdate: new Date(), source:"https://www.allrecipes.com/recipe/12682/apple-pie-by-grandma-ople/",type:"desserts"},
+    {id:4,name:'Omlet',img:'Omlet.jpeg',viewes:0,ingredients:["egges","oil"],lastupdate: new Date(),source:"https://www.food.com/recipe/omlet-526499",type:"Breakfast"},
+    {id:5,name:'Pancakes',img:'Pancakes.jpeg',viewes:0,ingredients:["flour","suger","oil"],lastupdate: new Date(), source:"https://www.allrecipes.com/recipe/21014/good-old-fashioned-pancakes/"},
+    {id:6,name:'Shakshuka',img:'recipe-imge.jpg',viewes:0,ingredients:["tomato","egges","pepper","salt"],lastupdate: new Date(), source:"https://cookieandkate.com/foolproof-shakshuka-recipe/"},
+    {id:7,name:'Lemon Pie',img:'lemonpie.jpeg',viewes:0,ingredients:["lemons","oil","flour","milk"],lastupdate: new Date(), source:"https://www.allrecipes.com/recipe/15093/grandmas-lemon-meringue-pie/",type:"desserts"},
+    {id:8,name:'Mafrum',img:'mafrum.jpg',viewes:0,ingredients:["Mince","potatos","egges","Breadcrumbs"],lastupdate: new Date(), source:"https://jamiegeller.com/recipes/libyan-mafrum-recipe/"}
 ]
 
 export default cardsData
