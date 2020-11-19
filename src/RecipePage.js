f6d08f4fd86ca9ea7ff15e64a97abbcb673a2d2c
asijanah <asafjanah@gmail.com>
2020-11-19 18:56:40 +0100

    Add some content and fix layout

--- src/RecipePage.js
+++ src/RecipePage.js
@@ -1,11 +1,11 @@
-import React from 'react';import { Link, useParams } from 'react-router-dom';
+import React from 'react'; import { Link, useParams } from 'react-router-dom';
 
 import cardsData from './cardsData';
 
 function RecipePage() {
 
     const params = useParams();
-    console.log(params);
+    console.log("param is: ",params);
     const recipe = cardsData.find(item => item.id == params.id);
     console.log(recipe);
 
@@ -14,16 +14,17 @@ function RecipePage() {
             <div className="jumbotron bg-light">
                 <h1 className="pb-4" >{recipe.name}</h1>
                 <div className="row">
-                    <div className="col-md-7">
-                        
+
+                    <div className="col-md-7 font-weight-bold">
+
                         <div className="row mb-1">
                             <p>Source:<Link>{recipe.source}</Link></p>
                         </div>
                         <div className="row mb-1">
-                            <p>Ingredients:</p>
+                            <p>Ingredients: {recipe.ingredients.join(" ,")}</p>
                         </div>
                         <div className="row mb-1">
-                            <p>Recipe Type:</p>
+                            <p>Recipe Type: {recipe.type}</p>
                         </div>
                         <div className="row mb-1">
                             <p>Suitable for:</p>
@@ -31,7 +32,7 @@ function RecipePage() {
                         <textarea rows="3" className="w-75 font-weight-light text-muted">Free explanation of how to prepare the recipe</textarea>
                     </div>
                     <div className="col-md-5">
-                        <img src={recipe.img}/>
+                        <img src={recipe.img} />
                     </div>
                 </div>
 
